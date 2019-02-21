3290524df4a0b78c139aea6c14895bacf0246330
OkaGouhei <g.oka1975@gmail.com>
2019-02-21 21:41:37 +0900

    質問

--- marubatsu.java
+++ marubatsu.java
@@ -94,7 +94,7 @@ public class Marubatsu{
       m++;
     }
   }
-  private static void banmen(String array[] ){
+  private static void banmen(String[][] array){
     for(int i = 0;i < 3;i++ ){
       for(int j = 0;j < 3;j++ ){
         System.out.print(array[i][j]);
